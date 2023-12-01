4f00db61b401d6eb5382fbf74cfc2a97faf22e40
Aaron Song <aaron@kmong.com>
2023-12-01 09:37:14 +0900

    ui: add constraint layout and button

--- app/src/main/java/song/aaron/composeconstraintlayout/MainActivity.kt
+++ app/src/main/java/song/aaron/composeconstraintlayout/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 import song.aaron.composeconstraintlayout.ui.theme.ComposeConstraintLayoutTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,4 +30,32 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
+
+@Composable
+fun MyButton(text: String, modifier: Modifier = Modifier) {
+    Button(
+        onClick = { /*TODO*/ },
+        modifier = modifier
+    ) {
+        Text(text = text)
+    }
+}
+
+@Composable
+fun MainScreen() {
+    ConstraintLayout(Modifier.size(width = 200.dp, height = 200.dp)) {
+        val (button1, button2, button3) = createRefs()
+
+        MyButton(text = "Button1", Modifier.constrainAs(button1) {
+            top.linkTo(parent.top, margin = 60.dp)
+            start.linkTo(parent.start, margin = 30.dp)
+        })
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun DefaultPreview() {
+    MainScreen()
 }
